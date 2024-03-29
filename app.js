f7be3bdbabf44c2370f07c0a5c914c30b6c4a425
Ashwitha <polasaniashwithareddy@gmail.com>
2024-03-29 13:52:25 -0500

    updated code

---
+++ app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var groceriesRouter = require('./routes/groceries'); //groceries endpoint added
+var gridRouter = require('./routes/grid'); //grid endpoint added
+var pickRouter = require('./routes/pick');//pick endpoint added
+var bikesRouter = require('./routes/bikes');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/groceries', groceriesRouter); 
+app.use('/grid', gridRouter); //grid endpoint added
+app.use('/pick', pickRouter); //pick endpoint added
+app.use('/bikes', bikesRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
