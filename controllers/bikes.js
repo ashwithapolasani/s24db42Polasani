9a80f714773e13a9e2136caa52a2ec2e02521591
Ashwitha <polasaniashwithareddy@gmail.com>
2024-04-12 02:12:38 -0500

    done

--- controllers/bikes.js
+++ controllers/bikes.js
@@ -2,29 +2,84 @@
 var bikes = require('../models/bikes');
  
 // Controller function to list all bikess
-exports.bikes_list = function (req, res) {
-    res.send('NOT IMPLEMENTED: bikes list');
+exports.bikes_list = async function (req, res) {
+    try {
+        thebikes = await Vehicles.find();
+        res.send(thebikes);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
  
 // Controller function to get details of a specific bikes
-exports.bikes_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: bikes detail: ' + req.params.id);
-};
+exports.bikes_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Vehicles.findById(req.params.id);
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
  
 // Controller function to create a new bikes
-exports.bikes_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: bikes create POST');
+exports.bikes_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new bikes();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.item_type = req.body.item_type;
+    document.item_name = req.body.item_name;
+    document.item_price = req.body.item_price;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
  
 // Controller function to delete a specific bikes
-exports.bikes_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: bikes delete DELETE ' + req.params.id);
-};
+exports.bikes_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await bikes.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
  
 // Controller function to update details of a specific bikes
-exports.bikes_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: bikes update PUT ' + req.params.id);
-};
+exports.bikes_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+   ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await bikes.findById(req.params.id)
+    // Do updates of properties
+    if(req.body.name)
+    toUpdate.item_type = req.body.item_type;
+    if(req.body.item_name) toUpdate.item_name = req.body.item_name;
+    if(req.body.item_price) toUpdate.item_price = req.body.item_price;
+    let result = await toUpdate.save();
+    console.log("Success " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   };
+
 
 exports.bikes_list = async function (req, res) {
     try {
