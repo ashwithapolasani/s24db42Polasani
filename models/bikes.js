e9ed295867f7c47b74f9f93033c76e9fb47023aa
Ashwitha <polasaniashwithareddy@gmail.com>
2024-04-17 03:05:29 -0500

    Added code

--- models/bikes.js
+++ models/bikes.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose")
 const bikesSchema = mongoose.Schema({
     item_type: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 30,
-        // Custom validator using RegExp
-        validate: {
-            validator: function (v) {
-                return /^[a-zA-Z0-9_ ]+$/.test(v); // Ensure length of vehicle between 3 and 30
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 30,
+            // Custom validator using RegExp
+            validate: {
+                validator: function (v) {
+                    return /^[a-zA-Z0-9_ ]+$/.test(v); // Ensure length of vehicle between 3 and 30
+                },
+                message: props => `${props.value} is not a valid username! Use only letters, numbers, and underscores.`,
             },
-            message: props => `${props.value} is not a valid username! Use only letters, numbers, and underscores.`,
-        },
     },
     item_name: {
         type: String,
