fd2ebbb5526fdca40c66631e9220fd4b22523a10
Ashwitha <polasaniashwithareddy@gmail.com>
2024-04-17 02:44:05 -0500

    Added code

--- models/bikes.js
+++ models/bikes.js
@@ -1,8 +1,40 @@
 const mongoose = require("mongoose")
 const bikesSchema = mongoose.Schema({
-item_type: String,
-item_name: String,
-item_price: Number
+item_type: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+    // Custom validator using RegExp
+    validate: {
+        validator: function (v) {
+            return /^[a-zA-Z0-9_ ]+$/.test(v); // Ensure length of vehicle between 3 and 30
+        },
+        message: props => `${props.value} is not a valid username! Use only letters, numbers, and underscores.`,
+    },
+},
+item_name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 30,
+    // Custom validator using RegExp
+    validate: {
+        validator: function (v) {
+            return /^[a-zA-Z0-9_ ]+$/.test(v); // Ensure length of vehicle between 3 and 30
+        },
+        message: props => `${props.value} is not a valid username! Use only letters, numbers, and underscores.`,
+    },
+},
+item_price: {
+    type: Number,
+    validate: {
+        validator: function(v) {
+            return v >= 0; // Ensure price is non-negative
+        },
+        message: props => `${props.value} is not a valid price! Price must be a non-negative number.`,
+    },
+},
 })
 module.exports = mongoose.model("bikes",
 bikesSchema)
--- routes/bikes.js
+++ routes/bikes.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const bikes_controlers= require('../controllers/bikes');
+
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('bikes', { title: 'Search Result bikes' });
@@ -11,13 +18,13 @@ router.get('/', bikes_controlers.bikes_view_all_Page );
 /* GET detail bikes page */
 router.get('/detail', bikes_controlers.bikes_view_one_Page);
 /* GET create bikes page */
-router.get('/create', bikes_controlers.bikes_create_Page)
+router.get('/create', secured, bikes_controlers.bikes_create_Page)
 
 /* GET create update page */
-router.get('/update', bikes_controlers.bikes_update_Page);
+router.get('/update', secured, bikes_controlers.bikes_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', bikes_controlers.bikes_delete_Page);
+router.get('/delete', secured, bikes_controlers.bikes_delete_Page);
 
 
 
